770c39fff7514ab9d3e577221c1cebedb762223a
Esteban Lopez <brandonlopezmonroy@gmail.com>
2023-08-02 12:39:29 -0500

    pasar todas las propiedades a name

--- back/src/models/Descuento.js
+++ back/src/models/Descuento.js
@@ -8,7 +8,7 @@ module.exports = (sequelize) => {
       primaryKey: true,
       autoIncrement: true,
     },
-    nombre: {
+    name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
