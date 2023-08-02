c99dcd433d0cf16784e1b8051b24665a2fce9bdd
danielpassantino <passantinodev@gmail.com>
2023-08-02 14:38:42 -0400

    dani

--- Frond/src/views/Catalogo/Catalogo.jsx
+++ Frond/src/views/Catalogo/Catalogo.jsx
@@ -12,7 +12,7 @@ const Catalogo = () => {
             <div className="grid grid-cols-2 pb-10 justify-items-center	">
               <p className="col-span-1 px-3 text-lg"><span className="font-bold">300</span> resultados para --</p>
               <select className="col-span-1 text-lg border-solid rounded border border-[255 255 255] px-2 py-[0.2rem] text-slate-400 focus:text-slate-950 focus:border-slate-950">
-                <option value="selecciona una opcion">selecciona una opcion</option>
+                <option value="selecciona una opcion">selecciona una opcion.</option>
                 <option value="a">a</option>
                 <option value="a">a</option>
               </select>
