e7577c8b06d1bbce51eeb1dbf9e43dbe4fb57691
abernix102 <nahuelo841@gmail.com>
2023-08-03 17:34:57 -0300

    cambios

--- Frond/src/App.js
+++ Frond/src/App.js
@@ -12,9 +12,7 @@ import Chatbot from "react-chatbot-kit";
 import Form from "./views/Form/Form";
 import Configs from "./components/ChatBot/Configs";
 import MessageParser from "./components/ChatBot/MessageParser";
-
 import Detail from "../src/views/Detail/Detail";
-=======
 import Dashboard from "./components/Dashboard/Dashboard";
 
 // import ActionProvider from "./components/ChatBot/ActionProvider";
