8adf8bb94969b2925f791ebefb025bfd9f6e8460
hdgomez8 <hdgomez8@hotmail.com>
2023-08-03 22:42:09 -0500

    Envio al servidor de imagenes

--- back/src/models/Categoria.js
+++ back/src/models/Categoria.js
@@ -1,27 +1,29 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 // Exportamos una función que define el modelo
 // Luego le inyectamos la conexión a sequelize.
 module.exports = (sequelize) => {
   // Defino el modelo
-  sequelize.define('categoria', {
-    id:{
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-  },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
+  sequelize.define(
+    "categoria",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      activa: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
     },
-    activa: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: true,
-    },
-  },
-  { tableName: 'categoria',timestamps: false }
+    { tableName: "categoria", timestamps: false }
   );
 };
