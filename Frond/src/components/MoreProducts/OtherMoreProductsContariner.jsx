e135103ef918d9097feb2f58bd51e49144678a5f
natisalvarez <naatisalvarez@gmail.com>
2023-08-10 14:06:53 +0200

    estilos terminados

--- Frond/src/components/MoreProducts/OtherMoreProductsContariner.jsx
+++ Frond/src/components/MoreProducts/OtherMoreProductsContariner.jsx
@@ -5,10 +5,10 @@ import "pure-react-carousel/dist/react-carousel.es.css";
 
 /* Install pure-react-carousel using -> npm i pure-react-carousel */
 
-const MoreProductsCardContainer = () => {
+const OtherMoreProductsContainer = () => {
   return (
     <div className="container mx-auto">
-      <h2 className="bg-customColor m-2 font-semibold inline-block text-white ml-10 py-1 px-4 items-center gap-2"> Más productos </h2>
+      <h2 className="bg-customColor m-2 font-semibold inline-block text-white ml-10 py-1 px-4 items-center gap-2"> Ofertas </h2>
       <div className="flex items-center justify-center w-full h-full py-24 sm:py-8 px-4">
         {/* Carousel for desktop and large size devices */}
         <CarouselProvider className="lg:block hidden" naturalSlideWidth={100} isIntrinsicHeight={true} totalSlides={12} visibleSlides={4} step={1} infinite={true}>
@@ -23,94 +23,6 @@ const MoreProductsCardContainer = () => {
                 <div id="slider" className="h-full flex lg:gap-8 md:gap-6 gap-14 items-center justify-start transition ease-out duration-700">
                   <Slide index={0}>
                     <div className="flex flex-shrink-0 relative w-full sm:w-auto">
-                      <img src= "https://i.ibb.co/YNczvML/img2632.jpg" alt="black chair and white table" className="object-cover object-center w-[320px] h-[220px]" />
-                      <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
-                        <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white"></h2>
-                        <div className="flex h-full items-end pb-6">
-                          <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white"></h3>
-                        </div>
-                      </div>
-                    </div>
-                  </Slide>
-                  <Slide index={1}>
-                    <div className="flex flex-shrink-0 relative w-full sm:w-auto">
-                      <img src="https://i.ibb.co/NKh10gM/img1299.jpg" alt="sitting area" className="object-cover object-center w-[320px] h-[220px]" />
-                      <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
-                        <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white"></h2>
-                        <div className="flex h-full items-end pb-6">
-                          <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white"></h3>
-                        </div>
-                      </div>
-                    </div>
-                  </Slide>
-                  <Slide index={2}>
-                    <div className="flex flex-shrink-0 relative w-full sm:w-auto">
-                      <img src="https://i.ibb.co/LZJ5ScX/img169.jpg" alt="sitting area" className="object-cover object-center w-[320px] h-[220px]" />
-                      <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
-                        <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white"></h2>
-                        <div className="flex h-full items-end pb-6">
-                          <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white"></h3>
-                        </div>
-                      </div>
-                    </div>
-                  </Slide>
-                  <Slide index={3}>
-                    <div className="flex flex-shrink-0 relative w-full sm:w-auto">
-                      <img src="https://i.ibb.co/c29F8j8/img261.jpg" alt="sitting area" className="object-cover object-center w-[320px] h-[220px]" />
-                      <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
-                        <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white"></h2>
-                        <div className="flex h-full items-end pb-6">
-                          <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white"></h3>
-                        </div>
-                      </div>
-                    </div>
-                  </Slide>
-                  <Slide index={4}>
-                    <div className="flex flex-shrink-0 relative w-full sm:w-auto">
-                      <img src="https://i.ibb.co/74xX5kQ/img271.jpg" alt="black chair and white table" className="object-cover object-center w-[320px] h-[220px]" />
-                      <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
-                        <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white"></h2>
-                        <div className="flex h-full items-end pb-6">
-                          <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white"></h3>
-                        </div>
-                      </div>
-                    </div>
-                  </Slide>
-                  <Slide index={5}>
-                    <div className="flex flex-shrink-0 relative w-full sm:w-auto">
-                      <img src="https://i.ibb.co/gZX0zvJ/img1384.jpg" alt="sitting area" className="object-cover object-center w-[320px] h-[220px]" />
-                      <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
-                        <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white"></h2>
-                        <div className="flex h-full items-end pb-6">
-                          <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white"></h3>
-                        </div>
-                      </div>
-                    </div>
-                  </Slide>
-                  <Slide index={6}>
-                    <div className="flex flex-shrink-0 relative w-full sm:w-auto">
-                      <img src="https://i.ibb.co/B2VnhML/img2693.jpg" alt="sitting area" className="object-cover object-center w-[320px] h-[220px]" />
-                      <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
-                        <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white"></h2>
-                        <div className="flex h-full items-end pb-6">
-                          <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white"></h3>
-                        </div>
-                      </div>
-                    </div>
-                  </Slide>
-                  <Slide index={7}>
-                    <div className="flex flex-shrink-0 relative w-full sm:w-auto">
-                      <img src="https://i.ibb.co/mBJBnBF/img306.jpg" alt="sitting area" className="object-cover object-center w-[320px] h-[220px]" />
-                      <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
-                        <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white"></h2>
-                        <div className="flex h-full items-end pb-6">
-                          <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white"></h3>
-                        </div>
-                      </div>
-                    </div>
-                  </Slide>
-                  <Slide index={8}>
-                    <div className="flex flex-shrink-0 relative w-full sm:w-auto">
                       <img src="https://i.ibb.co/ZX5Tqs4/img1260.jpg" alt="black chair and white table" className="object-cover object-center w-[320px] h-[220px]" />
                       <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
                         <h2 className="texlg:t-xl le leading-4 text-basealg:ding-tight text-white"></h2>
@@ -120,7 +32,7 @@ const MoreProductsCardContainer = () => {
                       </div>
                     </div>
                   </Slide>
-                  <Slide index={9}>
+                  <Slide index={1}>
                     <div className="flex flex-shrink-0 relative w-full sm:w-auto">
                       <img src="https://i.ibb.co/YNczvML/img2632.jpg" alt="sitting area" className="object-cover object-center w-[320px] h-[220px]" />
                       <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
@@ -131,7 +43,7 @@ const MoreProductsCardContainer = () => {
                       </div>
                     </div>
                   </Slide>
-                  <Slide index={10}>
+                  <Slide index={2}>
                     <div className="flex flex-shrink-0 relative w-full sm:w-auto">
                       <img src="https://i.ibb.co/0tsFck5/img2798.jpg" alt="sitting area" className="object-cover object-center w-[320px] h-[220px]" />
                       <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
@@ -142,7 +54,7 @@ const MoreProductsCardContainer = () => {
                       </div>
                     </div>
                   </Slide>
-                  <Slide index={11}>
+                  <Slide index={3}>
                     <div className="flex flex-shrink-0 relative w-full sm:w-auto">
                       <img src="https://i.ibb.co/FB9DG1p/img3428.jpg" alt="sitting area" className="object-cover object-center w-[320px] h-[220px]" />
                       <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
@@ -155,6 +67,95 @@ const MoreProductsCardContainer = () => {
                   </Slide>
                 </div>
               </Slider>
+
+              <Slide index={4}>
+                    <div className="flex flex-shrink-0 relative w-full sm:w-auto">
+                      <img src= "https://i.ibb.co/YNczvML/img2632.jpg" alt="black chair and white table" className="object-cover object-center w-[320px] h-[220px]" />
+                      <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
+                        <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white"></h2>
+                        <div className="flex h-full items-end pb-6">
+                          <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white"></h3>
+                        </div>
+                      </div>
+                    </div>
+                  </Slide>
+                  <Slide index={5}>
+                    <div className="flex flex-shrink-0 relative w-full sm:w-auto">
+                      <img src="https://i.ibb.co/NKh10gM/img1299.jpg" alt="sitting area" className="object-cover object-center w-[320px] h-[220px]" />
+                      <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
+                        <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white"></h2>
+                        <div className="flex h-full items-end pb-6">
+                          <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white"></h3>
+                        </div>
+                      </div>
+                    </div>
+                  </Slide>
+                  <Slide index={6}>
+                    <div className="flex flex-shrink-0 relative w-full sm:w-auto">
+                      <img src="https://i.ibb.co/LZJ5ScX/img169.jpg" alt="sitting area" className="object-cover object-center w-[320px] h-[220px]" />
+                      <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
+                        <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white"></h2>
+                        <div className="flex h-full items-end pb-6">
+                          <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white"></h3>
+                        </div>
+                      </div>
+                    </div>
+                  </Slide>
+                  <Slide index={7}>
+                    <div className="flex flex-shrink-0 relative w-full sm:w-auto">
+                      <img src="https://i.ibb.co/c29F8j8/img261.jpg" alt="sitting area" className="object-cover object-center w-[320px] h-[220px]" />
+                      <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
+                        <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white"></h2>
+                        <div className="flex h-full items-end pb-6">
+                          <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white"></h3>
+                        </div>
+                      </div>
+                    </div>
+                  </Slide>
+                  <Slide index={8}>
+                    <div className="flex flex-shrink-0 relative w-full sm:w-auto">
+                      <img src="https://i.ibb.co/74xX5kQ/img271.jpg" alt="black chair and white table" className="object-cover object-center w-[320px] h-[220px]" />
+                      <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
+                        <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white"></h2>
+                        <div className="flex h-full items-end pb-6">
+                          <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white"></h3>
+                        </div>
+                      </div>
+                    </div>
+                  </Slide>
+                  <Slide index={9}>
+                    <div className="flex flex-shrink-0 relative w-full sm:w-auto">
+                      <img src="https://i.ibb.co/gZX0zvJ/img1384.jpg" alt="sitting area" className="object-cover object-center w-[320px] h-[220px]" />
+                      <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
+                        <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white"></h2>
+                        <div className="flex h-full items-end pb-6">
+                          <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white"></h3>
+                        </div>
+                      </div>
+                    </div>
+                  </Slide>
+                  <Slide index={10}>
+                    <div className="flex flex-shrink-0 relative w-full sm:w-auto">
+                      <img src="https://i.ibb.co/B2VnhML/img2693.jpg" alt="sitting area" className="object-cover object-center w-[320px] h-[220px]" />
+                      <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
+                        <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white"></h2>
+                        <div className="flex h-full items-end pb-6">
+                          <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white"></h3>
+                        </div>
+                      </div>
+                    </div>
+                  </Slide>
+                  <Slide index={11}>
+                    <div className="flex flex-shrink-0 relative w-full sm:w-auto">
+                      <img src="https://i.ibb.co/mBJBnBF/img306.jpg" alt="sitting area" className="object-cover object-center w-[320px] h-[220px]" />
+                      <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
+                        <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white"></h2>
+                        <div className="flex h-full items-end pb-6">
+                          <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white"></h3>
+                        </div>
+                      </div>
+                    </div>
+                  </Slide>
             </div>
             <ButtonNext role="button" aria-label="slide forward" className="absolute z-30 right-0 mr-8 focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400" id="next">
               <svg width={8} height={14} viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -165,53 +166,7 @@ const MoreProductsCardContainer = () => {
         </CarouselProvider>
       </div>
     </div>
-  );
+  )
 }
 
-export default MoreProductsCardContainer;
-
-
-/* import { useSelector, useDispatch } from "react-redux";
-import MoreProductsCard from "./MoreProductsCard";
-import { useEffect } from "react";
-import { products } from "../../redux/actions";
-// Asegúrate de importar la acción adecuadamente
-
-const MoreProductsContainer = () => {
-    const dispatch = useDispatch;
-
-  // Utilizamos directamente el estado "stateProducts" proveniente de Redux
-const productos = useSelector((state)=> state.allProducts)
-useEffect(() => {
-    dispatch(products()) 
-}); // despacha cuando se monta
-
-  return (
-    <div className="grid md:grid-cols-3 lg:grid-cols-4 grid-auto-rows grid-rows-1 gap-5">
-      {productos.length>0? productos.map(({id, name, descripcion, precio_venta}) => {
-        return (
-          <MoreProductsCard
-            id={id}
-            key={id}
-            name={name}
-            descripcion={descripcion}
-            precio={precio_venta}
-          />
-        );
-      }): productos.productos?.map(({id, name, descripcion, precio_venta}) => {
-        return (
-          <MoreProductsCard
-            id={id}
-            key={id}
-            name={name}
-            descripcion={descripcion}
-            precio={precio_venta}
-          />
-        );
-      })}
-    </div>
-  );
-};
-
-export default MoreProductsContainer;
-*/
+export default OtherMoreProductsContainer;
--- Frond/src/views/Cart/AddToCart.jsx
+++ Frond/src/views/Cart/AddToCart.jsx
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { getCartProducts, addToCartFunction } from "../../redux/actions";
 import { useEffect } from "react";
 import greentick from '../../assets/img/greentick.png';
+import MoreProductsCardContainer from "../../components/MoreProducts/MoreProductsContainer";
+import OtherMoreProductsContainer from "../../components/MoreProducts/OtherMoreProductsContariner";
 
 let image = "https://cdn2.primor.eu/media/catalog/product/cache/8d3aba296f7a18b5251ee30fa5db42b2/0/M/0ML19241_1_1c53.webp";
 
@@ -31,21 +33,20 @@ const AddToCart = () => {
   return (
     <div className='flex flex-column items-center justify-center border border-black-500 border-5'>
       <div className='w-full h-full border border-black-500 border-500'>
-        <div className='flex m-10 rounded-lg shadow-2xl  p-20 justify-between items-center'>
+        <div className='flex m-10 rounded-lg shadow-2xl p-20 justify-between items-center'>
 
           {stateProducts.productos && stateProducts.productos.map((item) => (
             <p key={item.id}>{item.name.toUpperCase()}</p>
           ))}
-          <div className='flex items-center m-0'>
+          <div className='flex items-center'>
             <div className='relative'>
-              <img src={image /* stateProducts.imagen */} alt="Productimage" className='border border-green-500 w-20 h-20 object-cover rounded-full ml-10' />
+              <img src={image /* stateProducts.imagen */} alt="Productimage" className='border border-green-500 w-20 h-20 object-cover rounded-full ml-10 mr-2' />
               <img src={greentick} alt="tick-image" className='w-10 h-10 object-cover rounded-full absolute bottom-0 right-0 m-0' />
             </div>
-
           </div>
 
           <div>
-          <p className='text-1xl font-bold'>{(stateProducts.name)}</p>
+          <p className=' text-1xl font-bold'>{(stateProducts.name)}</p>
           <p className='text-green-500 font-semibold'>fue agregado al carrito exitosamente</p>
           </div>
 
@@ -63,7 +64,11 @@ const AddToCart = () => {
         </div>
 
         <div className='flex flex-row gap-2 m-10 rounded-lg bg-fuchsia-200 p-10 justify-center items-center'>
-          <h1> Acá va catalogo </h1>
+          <MoreProductsCardContainer />
+        </div>
+
+        <div className='flex flex-row gap-2 m-10 rounded-lg p-10 shadow-2xl justify-center items-center'>
+          <OtherMoreProductsContainer/>
         </div>
       </div>
     </div>
