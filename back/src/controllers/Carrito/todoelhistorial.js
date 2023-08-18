422616ce2f3d3dcab60555b21166b42142d5d69f
Esteban Lopez <brandonlopezmonroy@gmail.com>
2023-08-18 12:21:00 -0500

    historial

--- back/src/controllers/Carrito/todoelhistorial.js
+++ back/src/controllers/Carrito/todoelhistorial.js
@@ -1,22 +1,46 @@
-const { Carrito } = require('../../db');
+const { Carrito, Producto, Color } = require('../../db');
 
 module.exports = async () => {
-  try {
-    // Verificar si ya existe un carrito para el cliente
+  try {    
     const carritoExistente = await Carrito.findAll({
       where: {
-        pagado:true
+        pagado: true,
       },
     });
 
     if (!carritoExistente.length) {
-      throw new Error(`No existe un historial para el el cliente con ID ${clienteId}.`);
+      throw new Error(`No existe un historial`);
     }
 
-    carritoExistente.map(inventario => inventario.dataValues.id = `inv-${inventario.dataValues.id}`)
-    
 
-    return carritoExistente;
+    const productos = [];
+
+    for (const carrito of carritoExistente) {
+      for (const producto of carrito.dataValues.productos) {
+        const productoDetalles = await Producto.findByPk(producto.productoId, {
+          attributes: ['name', 'precio_venta'],
+        });
+        const colorDetalles = await Color.findByPk(producto.colorId, {
+          attributes: ['name'],
+        });
+
+        if (productoDetalles) {
+          productos.push({
+            colorId: producto.colorId,
+            colorName: colorDetalles.name,
+            cantidad: producto.cantidad,
+            productoId: producto.productoId,
+            productoName: productoDetalles.name,
+            productoPrecio: productoDetalles.precio_venta,
+            inventarioId: producto.inventarioId,
+            fechaCompra: carrito.dataValues.fechaCompra,
+          });
+        }
+      }
+    }
+
+
+    return productos;
   } catch (error) {
     console.error('Error al ver registro en el carrito:', error.message);
     throw error;
