35b57b60f4394da08a239b27565800e3e09e68a6
ivethglez80 <ivethglez80@gmail.com>
2023-08-21 13:11:01 -0300

    condiciione la renderizacion en cards conforme a search o filtros

--- Frond/src/components/CatalogoComponen/Cards.jsx
+++ Frond/src/components/CatalogoComponen/Cards.jsx
@@ -5,33 +5,24 @@ import Card from "./Card";
 const Cards = ({stateProducts}) => {
   // Utilizamos directamente el estado "stateProducts" proveniente de Redux
 const productosFiltrados = useSelector((state)=> state.productsFiltered)
-  return (
-    <div className="grid md:grid-cols-3 lg:grid-cols-4 grid-auto-rows grid-rows-1 gap-5">
-      {productosFiltrados.length>0? productosFiltrados.map(({id, name, descripcion, precio_venta,imagenPrincipal}) => {
-        return (
-          <Card
-            id={id}
-            key={id}
-            name={name}
-            descripcion={descripcion}
-            precio={precio_venta}
-            imagenPrincipal={imagenPrincipal}
-          />
-        );
-      }): stateProducts.productos?.map(({id, name, descripcion, precio_venta,imagenPrincipal}) => {
-        return (
-          <Card
-            id={id}
-            key={id}
-            name={name}
-            descripcion={descripcion}
-            precio={precio_venta}
-            imagenPrincipal={imagenPrincipal}
-          />
-        );
-      })}
-    </div>
-  );
+const searchResults = useSelector((state) => state.searchResults); 
+const productListToRender = searchResults.length > 0 ? searchResults : stateProducts.productos;
+return (
+  <div className="grid md:grid-cols-3 lg:grid-cols-4 grid-auto-rows grid-rows-1 gap-5">
+    {productListToRender.map(({ id, name, descripcion, precio_venta, imagenPrincipal }) => {
+      return (
+        <Card
+          id={id}
+          key={id}
+          name={name}
+          descripcion={descripcion}
+          precio={precio_venta}
+          imagenPrincipal={imagenPrincipal}
+        />
+      );
+    })}
+  </div>
+);
 };
 
 export default Cards;
